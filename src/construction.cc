a769f7ce9c015f7298c9c7d650a3ef4ce51cd458
dserafini <124274535+dserafini@users.noreply.github.com>
2024-10-19 15:25:37 +0200

    basic geant4 simulation from mustafa in v11

---
+++ src/construction.cc
@@ -0,0 +1,52 @@
+#include "construction.hh"
+
+MyDetectorConstruction::MyDetectorConstruction()
+{
+    G4cout << "MyDetectorConstruction::MyDetectorConstruction" << G4endl;
+}
+
+MyDetectorConstruction::~MyDetectorConstruction()
+{}
+
+G4VPhysicalVolume* MyDetectorConstruction::Construct()
+{
+  // Get nist material manager
+  G4NistManager* nist = G4NistManager::Instance();
+
+  // Envelope parameters
+  //
+  G4double env_sizeXY = 20*cm, env_sizeZ = 30*cm;
+  G4Material* env_mat = nist->FindOrBuildMaterial("G4_WATER");
+
+  // Option to switch on/off checking of volumes overlaps
+  //
+  G4bool checkOverlaps = true;
+
+  //
+  // World
+  //
+  G4double world_sizeXY = 1.2*env_sizeXY;
+  G4double world_sizeZ  = 1.2*env_sizeZ;
+  G4Material* world_mat = nist->FindOrBuildMaterial("G4_AIR");
+
+  auto solidWorld = new G4Box("World",                           // its name
+    0.5 * world_sizeXY, 0.5 * world_sizeXY, 0.5 * world_sizeZ);  // its size
+
+  auto logicWorld = new G4LogicalVolume(solidWorld,  // its solid
+    world_mat,                                       // its material
+    "World");                                        // its name
+
+  auto physWorld = new G4PVPlacement(nullptr,  // no rotation
+    G4ThreeVector(),                           // at (0,0,0)
+    logicWorld,                                // its logical volume
+    "World",                                   // its name
+    nullptr,                                   // its mother  volume
+    false,                                     // no boolean operation
+    0,                                         // copy number
+    checkOverlaps);                            // overlaps checking
+
+  //
+  //always return the physical World
+  //
+  return physWorld;
+}
