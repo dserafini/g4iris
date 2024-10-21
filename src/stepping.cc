7878aed2a984439f43ef031c0ea71edee2edc899
dserafini <124274535+dserafini@users.noreply.github.com>
2024-10-21 18:16:15 +0200

    remove hpge from stepping

--- src/stepping.cc
+++ src/stepping.cc
@@ -14,7 +14,6 @@ void MySteppingAction::UserSteppingAction(const G4Step *step)
 
     const G4LogicalVolume *logicalTablet = G4LogicalVolumeStore::GetInstance()->GetVolume("logicalTablet");
     const G4LogicalVolume *logicalPoint = G4LogicalVolumeStore::GetInstance()->GetVolume("logicalPoint");
-    const G4LogicalVolume *logicalHpge = G4LogicalVolumeStore::GetInstance()->GetVolume("logicalHpge");
 
     if(volume == logicalPoint && !fEventAction->isCrossed())
         fEventAction->Cross();
@@ -23,11 +22,5 @@ void MySteppingAction::UserSteppingAction(const G4Step *step)
     {
         G4double edep = step->GetTotalEnergyDeposit();
         fEventAction->AddEdep(edep);
-    }
-    
-    if(volume == logicalHpge)
-    {
-        G4double edep = step->GetTotalEnergyDeposit();
-        fEventAction->AddEdepHpge(edep);
     }
 }
